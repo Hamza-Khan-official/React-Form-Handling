5d25030d155813d3c1a4637ced38dea3318aea6d
Hamza-Khan-official <kh.hamza56789@gmail.com>
2026-01-17 00:28:52 +0500

    input value

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Login from './Components/Login/Login';
+import Login from './Components/Login/Login';
 import Input from './Screens/Login_screen/Login_screen.jsx'
 
 
--- src/Screens/Login_screen/Login_screen.jsx
+++ src/Screens/Login_screen/Login_screen.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import Input_ from '../../Components/Input/Input__.jsx'
 import styles from '../../Components/Input/Input.module.css'
+import Table from 'react-bootstrap/Table';
 
 
 const Login_screen = () => {
 
-    const [Login, setLogin] = useState ({
-        username:"",
-        userpassward:""
+    const [Login, setLogin] = useState({
+        username: "",
+        userpassward: ""
     })
 
     const [loginArray, SetloginArray] = useState([])
@@ -18,18 +19,58 @@ const Login_screen = () => {
         console.log(loginArray)
     }
 
-
     return (
-        <div>
+        <>
             <div className={styles.formContainer}>
                 <p className={styles.title}>Login</p>
 
-                <Input_ ONCHANGE={(e)=> setLogin({...Login, username: e.target.value})} label="Email" placholder="Enter a email..." type="email" />
-                <Input_ ONCHANGE={(e)=> setLogin({...Login, userpassward: e.target.value})} label="Passward" placholder="Enter a Passward..." type="password" />
+                {/* INput For Email */}
+                <Input_ ONCHANGE={(e) => {
+                    // inputval(e)
+                    setLogin({ ...Login, username: e.target.value })
+                }
+            }
+            label="Email" placholder="Enter a email..." type="email" value={Login.username} />
+
+                {/* INput For Passwards */}
+                <Input_ ONCHANGE={(e) => setLogin({ ...Login, userpassward: e.target.value })} label="Passward" placholder="Enter a Passward..." type="password" value={Login.userpassward} />
 
                 <button onClick={Login_func} className={styles.sign}>Sign in</button>
             </div>
-        </div>
+
+            <br /><br /><br />
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>{loginArray.username}</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Larry the Bird</td>
+                        <td>Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
     )
 }
 
